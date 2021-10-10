d78b259b38a4f9d98c57176f792c43ac024ba614
beslimir <marin.beslo@gmail.com>
2021-10-10 17:11:17 +0200

    Add @PerApplication scope for Driver

--- app/src/main/java/com/example/dagger2_again/dagger2/AppComponent.kt
+++ app/src/main/java/com/example/dagger2_again/dagger2/AppComponent.kt
@@ -2,15 +2,12 @@ package com.example.dagger2_again.dagger2
 
 import com.example.dagger2_again.car.Driver
 import dagger.Component
-import javax.inject.Singleton
-
-//TODO: Create a new scope, PerApplication, like PerActivity (it has the same effect like @Singleton)
 
 /**
  * Available through the whole lifetime of the app
  * **/
 
-@Singleton
+@PerApplication
 @Component(modules = [DriverModule::class])
 interface AppComponent {
 
--- app/src/main/java/com/example/dagger2_again/dagger2/DriverModule.kt
+++ app/src/main/java/com/example/dagger2_again/dagger2/DriverModule.kt
@@ -3,14 +3,13 @@ package com.example.dagger2_again.dagger2
 import com.example.dagger2_again.car.Driver
 import dagger.Module
 import dagger.Provides
-import javax.inject.Singleton
 
 @Module
 abstract class DriverModule {
 
     companion object {
 
-        @Singleton
+        @PerApplication
         @Provides
         fun provideDriver(): Driver {
             return Driver()
