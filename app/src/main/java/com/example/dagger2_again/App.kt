f4530b00cb78427e3e72d42a3b76f2523cb22336
beslimir <marin.beslo@gmail.com>
2021-10-10 15:00:03 +0200

    add comment to .App class

--- app/src/main/java/com/example/dagger2_again/App.kt
+++ app/src/main/java/com/example/dagger2_again/App.kt
@@ -4,6 +4,12 @@ import android.app.Application
 import com.example.dagger2_again.dagger2.CarComponent
 import com.example.dagger2_again.dagger2.DaggerCarComponent
 
+/**
+ * In order to have an application scope, we implement this Class and add it to Manifest in .name parameter.
+ * Now, we will have the same driver in the lifetime of the app, but the cars will instantiate again
+ * if we rotate the screen, for example, because car's scope isn't the same as the application's.
+ * **/
+
 class App: Application() {
 
     private lateinit var component: CarComponent
